db34df26346894fef5287539553c0d3c06b847ed
andreaswwjd <andreas.wwjd@gmail.com>
2017-03-06 14:06:07 +0100

    Swipe cards

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -117,7 +117,8 @@ var loadItems = function(items){
 			.html(function(article) { return '<div class="article"><div class="paper"><h1>'+article.title+'</h1></div><div class="paper" style="height: 333px;"><div class="thumbnail"><img src="'+article.thumbnail+'"><hr><p><i>'+article.description+'</i></p></div><div class="content" style="display: none"><p>'+article.read_more+'</p></div></div></div><div class="paper article_content">'+article.content_html+'</div>'})
 			// .style('transform', 'translateZ(-100px)')
 			.style('left', function(article,i) {
-				return i*card_width+'px';
+				this.left = i*card_width;
+				return this.left+'px';
 			})
 			.on('activate', function(article){
 				this.active = true;
@@ -154,144 +155,161 @@ var loadItems = function(items){
 			.on('click', function(d){
 				var self = this;
 				act_time = Date.now();
-			if(animation_turnoff){
-				!this.active ? d3.select(self).dispatch('activate') : d3.select(self).dispatch('deactivate'); 
-				d3.select(self).dispatch('no_animation');
-			}else{
-				
-				!this.active ? d3.select(self).dispatch('activate') : 'Deactivation occures later at end of animation'; 
+				if(animation_turnoff){
+					!this.active ? d3.select(self).dispatch('activate') : d3.select(self).dispatch('deactivate'); 
+					d3.select(self).dispatch('no_animation');
+				}else{
+					
+					!this.active ? d3.select(self).dispatch('activate') : 'Deactivation occures later at end of animation'; 
 
-				//Shadows
-				d3.select(self).selectAll('div.paper')
-					.transition().duration(flip_time)
-					.on('start', function(){
-						self.opening = !self.isOpen;
-						self.closing = self.isOpen;
-						d3.select(self).dispatch('blur')
+					//Shadows
+					d3.select(self).selectAll('div.paper')
+						.transition().duration(flip_time)
+						.on('start', function(){
+							self.opening = !self.isOpen;
+							self.closing = self.isOpen;
+							d3.select(self).dispatch('blur')
 
-						if( self.opening ){
-							overlay.style('z-index', 1).style('opacity', 0.6);
-							d3.select(self).attr('class', 'card active');
-						}else{
-							overlay.style('opacity', 0);
-							d3.select(self).attr('class', 'card active');
-						}	
-					})
-					// .html('<div class="thumbnail"></div><hr>')
-					.styleTween("background", function() {
-					    var i = d3.interpolate('#ffffff','#949494');
-					    return function(t) {
-					        return i((1-Math.cos(t*2*Math.PI))/2); 
-					    };
-					})
-					.styleTween("box-shadow", function() { 
-						return function(){ return self.opening ? "10px 23px 50px -10px rgba(0, 0, 0, 0.26)" : "0 3px 6px rgba(0, 0, 0, 0.16)"; } 
-					});
-				//Img
-				d3.select(self).selectAll('img')
-					.transition().duration(flip_time)
-					.styleTween("-webkit-filter", function(){
-						var br = d3.interpolate('brightness(100%)', 'brightness(5%)');
-						return function(t) {
-					        return br((1-Math.cos(t*2*Math.PI))/2); 
-					    };
-					});
-				//Content
-				d3.select(self).selectAll('div.thumbnail')
-					.transition().duration(flip_time)
-					.styleTween("display", function(){
-						return function(t) {
-					        return t>0.5&&self.opening || (1-t)>0.5&&self.closing ? 'none': 'block';
-					    };
-					});
-				d3.select(self).selectAll('div.content')
-					.transition().duration(flip_time)
-					.styleTween("display", function(){
-						return function(t) {
-					        return t>0.5&&self.opening || (1-t)>0.5&&self.closing ? 'block': 'none';
-					    };
-					});
+							if( self.opening ){
+								overlay.style('z-index', 1).style('opacity', 0.6);
+								d3.select(self).attr('class', 'card active');
+							}else{
+								overlay.style('opacity', 0);
+								d3.select(self).attr('class', 'card active');
+							}	
+						})
+						// .html('<div class="thumbnail"></div><hr>')
+						.styleTween("background", function() {
+						    var i = d3.interpolate('#ffffff','#949494');
+						    return function(t) {
+						        return i((1-Math.cos(t*2*Math.PI))/2); 
+						    };
+						})
+						.styleTween("box-shadow", function() { 
+							return function(){ return self.opening ? "10px 23px 50px -10px rgba(0, 0, 0, 0.26)" : "0 3px 6px rgba(0, 0, 0, 0.16)"; } 
+						});
+					//Img
+					d3.select(self).selectAll('img')
+						.transition().duration(flip_time)
+						.styleTween("-webkit-filter", function(){
+							var br = d3.interpolate('brightness(100%)', 'brightness(5%)');
+							return function(t) {
+						        return br((1-Math.cos(t*2*Math.PI))/2); 
+						    };
+						});
+					//Content
+					d3.select(self).selectAll('div.thumbnail')
+						.transition().duration(flip_time)
+						.styleTween("display", function(){
+							return function(t) {
+						        return t>0.5&&self.opening || (1-t)>0.5&&self.closing ? 'none': 'block';
+						    };
+						});
+					d3.select(self).selectAll('div.content')
+						.transition().duration(flip_time)
+						.styleTween("display", function(){
+							return function(t) {
+						        return t>0.5&&self.opening || (1-t)>0.5&&self.closing ? 'block': 'none';
+						    };
+						});
 
-				// Transform
-				d3.select(self)
-					// .attr('class', 'card active')
-					.transition().duration(flip_time)
-					.styleTween("transform", function() {
-						var tf_ry = d3.interpolate(0, 180);
-						var tf_tz = d3.interpolate(0, 30);
-						// var tf_sc = d3.interpolate(1, 1.2);
-					    return function(t) {
-					    	var rt = t>0.5 ? t-1 : t;
-					    	// var xt = t<0.5? 0 : t*2-1;
-					    	// var xxt = t<0.5? t*2 : 1;
-					        return self.opening ? 
-					        	"rotateY(" + tf_ry(rt) + "deg) translateZ("+tf_tz(t)+"px)" : //open
-					        	"rotateY(" + tf_ry(rt*-1) + "deg) translateZ("+tf_tz(1-t)+"px)" ; //close
+					// Transform
+					d3.select(self)
+						// .attr('class', 'card active')
+						.transition().duration(flip_time)
+						.styleTween("transform", function() {
+							var tf_ry = d3.interpolate(0, 180);
+							var tf_tz = d3.interpolate(0, 30);
+							// var tf_sc = d3.interpolate(1, 1.2);
+						    return function(t) {
+						    	var rt = t>0.5 ? t-1 : t;
+						    	// var xt = t<0.5? 0 : t*2-1;
+						    	// var xxt = t<0.5? t*2 : 1;
+						        return self.opening ? 
+						        	"rotateY(" + tf_ry(rt) + "deg) translateZ("+tf_tz(t)+"px)" : //open
+						        	"rotateY(" + tf_ry(rt*-1) + "deg) translateZ("+tf_tz(1-t)+"px)" ; //close
 
-					        	// "rotateY(" + tf_ry(rt) + "deg) scale("+tf_sc(t)+")" : //open
-					        	// "rotateY(" + tf_ry(rt*-1) + "deg) scale("+tf_sc(1-t)+")" ; //close
-					    };
-					})
-					.styleTween("left", function(d,i) {
-						var tx = d3.interpolate(d.index*card_width, open_card_x);
-					    return function(t) {
-					        return self.opening ? 
-					        	tx(t)+"px":
-					        	tx(1-t)+"px";
-					    };
-					})
-					.on('end', function() {
-						act_time = Date.now()-act_time;
-						if (act_time>flip_time*1.2) {
-							animation_turnoff = confirm(act_time+'ms: Wow, that was hacky.. Wanna turn off transitions?');;
-						}
-						self.opening = false;
-						self.closing = false;
-						self.isOpen = !self.isOpen;
-						if(!self.isOpen){
-							d3.select(self).attr('class', 'card');
-							overlay.style('z-index', 0).style('opacity', 0);
-							d3.select(self).dispatch('deactivate')
-						}else{
-							d3.select(self).attr('class', 'card active open');
-						}
-					});
-			}
+						        	// "rotateY(" + tf_ry(rt) + "deg) scale("+tf_sc(t)+")" : //open
+						        	// "rotateY(" + tf_ry(rt*-1) + "deg) scale("+tf_sc(1-t)+")" ; //close
+						    };
+						})
+						.styleTween("left", function(d,i) {
+							var tx = d3.interpolate(this.left, open_card_x);
+						    return function(t) {
+						        return self.opening ? 
+						        	tx(t)+"px":
+						        	tx(1-t)+"px";
+						    };
+						})
+						.on('end', function() {
+							act_time = Date.now()-act_time;
+							if (act_time>flip_time*1.2) {
+								animation_turnoff = confirm(act_time+'ms: Wow, that was hacky.. Wanna turn off transitions?');;
+							}
+							self.opening = false;
+							self.closing = false;
+							self.isOpen = !self.isOpen;
+							if(!self.isOpen){
+								d3.select(self).attr('class', 'card');
+								overlay.style('z-index', 0).style('opacity', 0);
+								d3.select(self).dispatch('deactivate')
+							}else{
+								d3.select(self).attr('class', 'card active open');
+							}
+						});
+				}
 			});
-		var touchstart = {x:0,y:0};
-		var direction = undefined;
-		d3.select(this).selectAll('article.card')
+		d3.select(this)
 			.on('touchstart', function(){
-				touchstart = {
-					x: d3.event.touches[0].clientX,
-					y: d3.event.touches[0].clientY
-				};
+				!this.touchstart ? this.touchstart = {moveX: 0}: 'pass';
+				this.touchstart.x = d3.event.touches[0].clientX;
+				this.touchstart.y = d3.event.touches[0].clientY;
+				this.touchstart.dir = undefined;
+				console.log(this.touchstart)
 			})
 			.on('touchmove', function(){
-				var moveX = d3.event.touches[0].clientX - touchstart.x;
-				var moveY = d3.event.touches[0].clientY - touchstart.y;
-				if(!direction && moveX*moveX > 25){
-					direction = 'x';
-				}
-				if(!direction && moveY*moveY > 25){
-					direction = 'y';
-				}
-				if(direction == 'x'){
+				// console.log(d3.event.touches[0].clientX - this.touchstart.x + (this.touchstart.moveX || 0))
+				var moveX = d3.event.touches[0].clientX - this.touchstart.x;
+				var moveY = d3.event.touches[0].clientY - this.touchstart.y;
+				if(!this.touchstart.dir && moveX*moveX > 25){ this.touchstart.dir = 'x'; }
+				if(!this.touchstart.dir && moveY*moveY > 25){ this.touchstart.dir = 'y'; }
+				// moveX += this.touchstart.moveX;
+				if(this.touchstart.dir == 'x'){
 					d3.event.preventDefault();
-					d3.select('#'+section.id).selectAll('.articles').style('left', moveX+'px');
-					console.log(('#'+section.id));
+					// d3.select('#'+section.id).selectAll('.card').style('margin-left', (moveX + this.touchstart.moveX || moveX)+'px');
+					d3.select('#'+section.id).selectAll('.card').style('left', function(){console.log(this.left); return (this.left + moveX )+'px'});
 				}
 			})
 			.on('touchend', function(){
-				if(direction == 'x'){
-					var moveX = d3.event.changedTouches[0].clientX - touchstart.x;
-					console.log(Math.round(moveX/card_width))
-					if(moveX*moveX> 100*100){
-						var n = Math.round(moveX/card_width)*card_width;
+				if(this.touchstart.dir == 'x'){
+					console.log(d3.event)
+					var moveX = d3.event.changedTouches[0].clientX - this.touchstart.x;
 
-						d3.select('#'+section.id).selectAll('.articles').style('left', function(){return n ? n+'px' : '0px'});
-					}
-					direction = undefined;
+					// var moveX = d3.event.changedTouches[0].clientX - this.touchstart.x + this.touchstart.moveX;
+					console.log(Math.round(moveX/card_width))
+					// if(moveX*moveX> 100*100){
+						var n = Math.round(moveX/card_width);
+
+						d3.select('#'+section.id).selectAll('.card').transition().duration(300).styleTween('left', function(){
+							var l = d3.interpolate(this.left+moveX, this.left+(n*card_width));
+							this.left += n*card_width;
+						    return function(t) {
+						        return l(t)+"px";
+						    };
+						});
+						// d3.select('#'+section.id).selectAll('.card').style('left', function(){ return this.left +'px'});
+					// }else{
+					// 	// d3.select('#'+section.id).selectAll('.card').style('left', function(){ this.left += moveX; return this.left +'px'});
+
+					// 	d3.select('#'+section.id).selectAll('.card').transition().duration(300).styleTween('left', function(){
+					// 		var l = d3.interpolate(this.left+moveX, this.left);
+					// 	    return function(t) {
+					// 	        return l(t)+"px";
+					// 	    };
+					// 	});
+					// }
+					// this.touchstart.moveX = moveX;
+
 				}
 			});
 	});
